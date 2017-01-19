a73d97e54a514c6428ade115dacba7e6bc8c0808
杨振甫 <1044818609@qq.com>
2017-01-19 11:28:05 +0800

    yzf修改物料对接数据中心接口

--- trfc-web/src/main/java/com/tianrui/web/api/datacenter/action/ApiMaterielAction.java
+++ trfc-web/src/main/java/com/tianrui/web/api/datacenter/action/ApiMaterielAction.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,6 +24,8 @@ import com.tianrui.web.smvc.ApiParamRawType;
  * @author Yangzhenfu
  * @createtime 2017年1月18日 下午16:44:30
  */
+@Controller
+@RequestMapping("api/dc/materiel")
 public class ApiMaterielAction {
 	private Logger log = LoggerFactory.getLogger(ApiCustomerAction.class);
 	@Autowired
