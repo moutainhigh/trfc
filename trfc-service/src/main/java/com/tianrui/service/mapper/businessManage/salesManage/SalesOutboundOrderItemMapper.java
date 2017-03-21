4353905f6d7b97c91fa682481d3f7438e21d1c41
杨振甫 <1044818609@qq.com>
2017-03-21 13:52:58 +0800

    销售出库单数据回传接口

---
+++ trfc-service/src/main/java/com/tianrui/service/mapper/businessManage/salesManage/SalesOutboundOrderItemMapper.java
@@ -0,0 +1,23 @@
+package com.tianrui.service.mapper.businessManage.salesManage;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.tianrui.service.bean.businessManage.salesManage.SalesOutboundOrderItem;
+
+public interface SalesOutboundOrderItemMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(SalesOutboundOrderItem record);
+
+    int insertSelective(SalesOutboundOrderItem record);
+
+    SalesOutboundOrderItem selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(SalesOutboundOrderItem record);
+
+    int updateByPrimaryKey(SalesOutboundOrderItem record);
+
+	List<SalesOutboundOrderItem> selectByOrderIds(@Param("ids") List<String> ids);
+}
