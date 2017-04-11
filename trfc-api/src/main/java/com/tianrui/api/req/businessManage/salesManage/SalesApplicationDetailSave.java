b08fb5dad2a2f8fa03eadb25a2e73fa203ccb6d0
zhanggaohao <zhanggaohao@hotmail.com>
2017-04-11 16:32:59 +0800

    zhanggh  修改bug

--- trfc-api/src/main/java/com/tianrui/api/req/businessManage/salesManage/SalesApplicationDetailSave.java
+++ trfc-api/src/main/java/com/tianrui/api/req/businessManage/salesManage/SalesApplicationDetailSave.java
@@ -1,28 +1,37 @@
 package com.tianrui.api.req.businessManage.salesManage;
 
 public class SalesApplicationDetailSave {
+	//主键id
     private String id;
-
+    //销售申请单id
     private String salesid;
-
+    //物料id
     private String materielid;
-
+    //物料名称
     private String materielname;
-
+    //仓库id
     private String warehouseid;
-
+    //仓库名称
     private String warehousename;
-
+    //单位 default:吨
     private String unit;
-    
+    //数量
     private Double salessum;
-
+    //余量
+    private Double margin;
+    //出库占用量
+    private Double storagequantity;
+    //未出库占用量
+    private Double unstoragequantity;
+    //预提占用
+    private Double pretendingtake;
+    //含税单价
     private Double taxprice;
-
+    //不含税单价
     private Double untaxprice;
-
+    //税率
     private Double taxrate;
-
+    //备注
     private String remarks;
 
     public String getId() {
@@ -89,7 +98,63 @@ public class SalesApplicationDetailSave {
         this.salessum = salessum;
     }
 
-    public Double getTaxprice() {
+    /**
+	 * @return the margin
+	 */
+	public Double getMargin() {
+		return margin;
+	}
+
+	/**
+	 * @return the storagequantity
+	 */
+	public Double getStoragequantity() {
+		return storagequantity;
+	}
+
+	/**
+	 * @return the unstoragequantity
+	 */
+	public Double getUnstoragequantity() {
+		return unstoragequantity;
+	}
+
+	/**
+	 * @return the pretendingtake
+	 */
+	public Double getPretendingtake() {
+		return pretendingtake;
+	}
+
+	/**
+	 * @param margin the margin to set
+	 */
+	public void setMargin(Double margin) {
+		this.margin = margin;
+	}
+
+	/**
+	 * @param storagequantity the storagequantity to set
+	 */
+	public void setStoragequantity(Double storagequantity) {
+		this.storagequantity = storagequantity;
+	}
+
+	/**
+	 * @param unstoragequantity the unstoragequantity to set
+	 */
+	public void setUnstoragequantity(Double unstoragequantity) {
+		this.unstoragequantity = unstoragequantity;
+	}
+
+	/**
+	 * @param pretendingtake the pretendingtake to set
+	 */
+	public void setPretendingtake(Double pretendingtake) {
+		this.pretendingtake = pretendingtake;
+	}
+
+	public Double getTaxprice() {
         return taxprice;
     }
 
