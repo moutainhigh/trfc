b86790ebc8d2b330ae6f1f87df3278739d9ad2e4
李鑫鹏 <283534181@qq.com>
2017-12-02 14:22:07 +0800

    1

---
+++ trfc-api/src/main/java/com/tianrui/api/intf/businessManage/report/IOutPoundService.java
@@ -0,0 +1,17 @@
+package com.tianrui.api.intf.businessManage.report;
+
+import java.util.List;
+
+import com.tianrui.api.req.businessManage.report.InOutDaoPoundQuery;
+import com.tianrui.api.resp.businessManage.report.InOutDaoPoundResp;
+import com.tianrui.smartfactory.common.vo.PaginationVO;
+
+/**
+ * 其他入库逐车明细
+ * @author lenovo
+ *
+ */
+public interface IOutPoundService {
+	PaginationVO<InOutDaoPoundResp> page(InOutDaoPoundQuery query) throws Exception;
+	List<InOutDaoPoundResp> list(InOutDaoPoundQuery query) throws Exception;
+}
